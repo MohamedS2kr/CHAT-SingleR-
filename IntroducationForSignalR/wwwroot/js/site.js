93fe4839816f7d49ecd3372fc03d5d4f4c16350a
Mohamed S2kr <75217623+MohamedS2kr@users.noreply.github.com>
2024-09-24 01:25:18 +0300

    Add project files.

---
+++ IntroducationForSignalR/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")  // Hub endpoint defined in the server
+    .build();
+connection.on("ReceiveMessage", function (fromuser, message) {
+    var msg = fromuser + ": " + message;
+    var li = document.createElement("li");
+    li.txtcontect = msg;
+    $("#list").prepend(li);
+});
+
+connection.start().catch(err => console.error(err));
+
+$("#btnSend").on("click", function () {
+    var FromUser = $("#txtUser").val();
+    var Message = $("#txtMessage").val();
+
+    connection.invoke("SendMessage", FromUser, Message)
+});
